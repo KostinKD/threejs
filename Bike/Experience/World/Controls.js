c873e0a72ab60597f1285bfc103a0e359d0a8971
KostinKD <omroners@gmail.com>
2022-11-12 22:18:09 +0300

    Working on curves and control camera

--- Bike/Experience/World/Controls.js
+++ Bike/Experience/World/Controls.js
@@ -1,6 +1,7 @@
 import Experience from "../Experience.js";
 import * as THREE from 'three'
 import scene from "three/addons/offscreen/scene.js";
+import GSAP from 'gsap'
 
 
 export default class Controls{
@@ -9,10 +10,21 @@ export default class Controls{
         this.scene = this.experience.scene
         this.resources = this.experience.resources
         this.time = this.experience.time
+        this.camera = this.experience.camera
 
+        this.lerp = {
+            current: 1,
+            target: 0,
+            ease: 0.1,
+        }
 
+        this.dummyCurve = new THREE.Vector3(0,0,0)
+        this.progress = 0
+
+        this.position = new THREE.Vector3(0,0,0)
 
         this.setPath()
+        this.onWheel();
     }
 
     setPath(){
@@ -24,6 +36,9 @@ export default class Controls{
             new THREE.Vector3(10,0,10)
         ], true);
 
+        this.curve.getPointAt(this.progress,this.dummyCurve)
+        this.camera.OrthographicCamera.position.copy(this.dummyCurve)
+
         const points = this.curve.getPoints(50);
         const geometry = new THREE.BufferGeometry().setFromPoints(points)
 
@@ -34,10 +49,43 @@ export default class Controls{
         this.scene.add(curveObject)
 
     }
+
+    onWheel(){
+        window.addEventListener('wheel', (e)=>{
+            console.log(e)
+            if (e.deltaY > 0){
+                this.lerp.target += 0.1
+                this.back = true
+            }else {
+                this.lerp.target -= 0.1;
+                this.back = false
+            }
+        })
+    }
     resize(){
 
     }
     update(){
+
+        this.lerp.current = GSAP.utils.interpolate(
+            this.lerp.current,
+            this.lerp.target,
+            this.lerp.ease
+        );
+        if (this.back){
+            this.lerp.target -= 0.001
+        } else {
+            this.lerp.target += 0.001
+        }
+
+        this.lerp.target = GSAP.utils.clamp(0,1, this.lerp.target)
+        this.lerp.current = GSAP.utils.clamp(0,1, this.lerp.current)
+        this.curve.getPointAt(this.lerp.current,this.position)
+        // this.progress -= 0.01
+        // this.progress -= this.w
+
+        this.camera.OrthographicCamera.position.copy(this.position)
+
     }
 
 }
