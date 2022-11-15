7a2283964e14666865f2a8f7162b3277f59c5cb7
KD <omroners@gmail.com>
2022-11-15 11:54:05 +0300

    Working on animations border cards

--- Bike/Experience/World/Controls.js
+++ Bike/Experience/World/Controls.js
@@ -154,7 +154,51 @@ export default class Controls{
             },
 
             all:  ()=>{
+                this.sections = document.querySelectorAll('.section')
+                this.sections.forEach((section)=>{
+                    this.progressWrapper = section.querySelector('.progress-wrapper')
+                    this.progressBar = section.querySelector('.progress-bar')
 
+                    if (section.classList.contains("right")) {
+                        GSAP.to(section, {
+                            borderTopLeftRadius: 10,
+                            scrollTrigger: {
+                                trigger: section,
+                                start: "top bottom",
+                                end: "top top",
+                                scrub: 0.6,
+                            },
+                        });
+                        GSAP.to(section, {
+                            borderBottomLeftRadius: 700,
+                            scrollTrigger: {
+                                trigger: section,
+                                start: "bottom bottom",
+                                end: "bottom top",
+                                scrub: 0.6,
+                            },
+                        });
+                    } else {
+                        GSAP.to(section, {
+                            borderTopRightRadius: 10,
+                            scrollTrigger: {
+                                trigger: section,
+                                start: "top bottom",
+                                end: "top top",
+                                scrub: 0.6,
+                            },
+                        });
+                        GSAP.to(section, {
+                            borderBottomRightRadius: 700,
+                            scrollTrigger: {
+                                trigger: section,
+                                start: "bottom bottom",
+                                end: "bottom top",
+                                scrub: 0.6,
+                            },
+                        });
+                    }
+                })
 
             //    Mini Platform Anim
                 this.secondPartTimeLine = new GSAP.timeline({
