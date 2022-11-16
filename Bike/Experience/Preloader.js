b0be24863789199e023bc8ec380c25a730a6cfae
KD <omroners@gmail.com>
2022-11-16 13:17:31 +0300

    Continuing work on Preloader

--- Bike/Experience/Preloader.js
+++ Bike/Experience/Preloader.js
@@ -72,7 +72,7 @@ export default class Preloader extends EventEmitter{
                     x: 0,
                     y: 0,
                     // ease: 'back.out(2.5)',
-                    ease: 'power1.out',
+                    // ease: 'power1.out',
                     // duration: 0.7,
                     // onComplete: resolve,
                 }, 'same').to(this.roomChildren.cube.rotation, {
@@ -82,11 +82,15 @@ export default class Preloader extends EventEmitter{
                     y: 10,
                     z: 10
                 }, 'same').to(this.camera.OrthographicCamera.position, {
-                    y: 6.5,
+                    y: 3.5,
                 }, 'same').to(this.roomChildren.cube.position, {
                     x: 0.638711,
                     y: 8.5618,
                     z: 1.3243
+
+                    // x: 0,
+                    // y: 0,
+                    // z: 1
                 }, 'same').set(this.roomChildren.body.scale, {
                     x: 1,
                     y: 1,
@@ -95,18 +99,71 @@ export default class Preloader extends EventEmitter{
                     x: 0,
                     y: 0,
                     z: 0
-                })
-            } else {
-                this.secondTimeline.to(this.room.position, {
-                    z: 0,
-                    x: 0,
-                    y: 0,
-                    ease: 'back.out(2.5)',
-                    duration: 0.7,
+                }).to(this.roomChildren.aquarium.scale,{
+                    x: 1,
+                    y: 1,
+                    z: 1,
+                    ease: 'back.out(2.2)',
+                    duration: 0.5
+                }).to(this.roomChildren.clock.scale,{
+                    x: 1,
+                    y: 1,
+                    z: 1,
+                    ease: 'back.out(2.2)',
+                    duration: 0.5
+                }).to(this.roomChildren.shelves.scale,{
+                    x: 1,
+                    y: 1,
+                    z: 1,
+                    ease: 'back.out(2.2)',
+                    duration: 0.5
+                }).to(this.roomChildren.floor_items.scale,{
+                    x: 1,
+                    y: 1,
+                    z: 1,
+                    ease: 'back.out(2.2)',
+                    duration: 0.5
+                }).to(this.roomChildren.desks.scale,{
+                    x: 1,
+                    y: 1,
+                    z: 1,
+                    ease: 'back.out(2.2)',
+                    duration: 0.5
+                }).to(this.roomChildren.table_stuff.scale,{
+                    x: 1,
+                    y: 1,
+                    z: 1,
+                    ease: 'back.out(2.2)',
+                    duration: 0.5
+                }).to(this.roomChildren.computer.scale,{
+                    x: 1,
+                    y: 1,
+                    z: 1,
+                    ease: 'back.out(2.2)',
+                    duration: 0.5
+                }).to(this.roomChildren.chair.scale,{
+                    y: 1,
+                    x: 1,
+                    z: 1,
+                    ease: 'back.out(2.2)',
+                    duration: 0.5
+                }).to(this.roomChildren.chair.rotation,{
+                    y: 4 * Math.PI + Math.PI / 4,
+                    ease: 'power1.out',
+                    duration: 1,
                     onComplete: resolve,
-
                 })
             }
+            // else {
+            //     this.secondTimeline.to(this.room.position, {
+            //         z: 0,
+            //         x: 0,
+            //         y: 0,
+            //         ease: 'back.out(2.5)',
+            //         duration: 0.7,
+            //         onComplete: resolve,
+            //     })
+            // }
         })
 
     }
@@ -115,17 +172,39 @@ export default class Preloader extends EventEmitter{
 
     onScroll(e){
         if (e.deltaY > 0){
-            window.removeEventListener('wheel', this.scrollOnceEvent)
+            this.removeEventListeners()
             this.playSecondIntro()
 
         }
     }
+    onTouch(e){
+        console.log(e)
+        this.initialY = e.touches[0].clientY
+    }
+    onTouchMove(e){
+        let currentY = e.touches[0].clientY
+        let difference = this.initialY - currentY
+        if (difference > 0 ){
+            console.log('swipeed up')
+            this.removeEventListeners()
+            this.playSecondIntro()
+        }
+        this.initialY = null
+    }
 
+    removeEventListeners(){
+        window.removeEventListener('wheel', this.scrollOnceEvent)
+        window.removeEventListener('touchstart', this.touchStart)
+        window.removeEventListener('touchmove', this.touchMove)
+    }
     async playIntro(){
         await this.firstIntro()
-        console.log('continuing')
         this.scrollOnceEvent = this.onScroll.bind(this)
+        this.touchStart = this.onTouch.bind(this)
+        this.touchMove = this.onTouchMove.bind(this)
         window.addEventListener('wheel', this.scrollOnceEvent)
+        window.addEventListener('touchstart', this.touchStart)
+        window.addEventListener('touchmove', this.touchMove)
     }
     async playSecondIntro(){
         await this.secondIntro()
